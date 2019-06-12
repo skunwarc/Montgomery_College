0e20d62ac1f93277ce33cca943e326c2b2fd453f
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-06-12 10:07:28 -0400

    Create MathTwo.java

---
+++ MathTwo.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int mynumber=2;
+        
+        int myothernumber=1.7938;
+        
+    System.out.println(mynumber);
+    
+     System.out.println(myothernumber);
+    }
+}
