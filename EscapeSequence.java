9095a977d42c0c720db00f1e96af9340f2207d91
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-06-13 11:01:58 -0400

    Create EscapeSequence.java

---
+++ EscapeSequence.java
@@ -0,0 +1,28 @@
+public class EscapeSequences {
+
+    public static void main(String[] args) {
+        System.out.println("********");
+        System.out.println("*   " + "   *");
+        System.out.println("*   " + "   *");
+        System.out.println("*   " + "   *");
+        System.out.println("*   " + "   *");
+        System.out.println("********");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("   ***");
+        System.out.println("  *    *");
+        System.out.println(" *      *");
+        System.out.println("  *     *");
+        System.out.println("   *   * ");
+        System.out.println("    ***");
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("    * ");
+        System.out.println("   *  * ");
+        System.out.println("  *    * ");
+        System.out.println("   *   *  ");
+        System.out.println("     * ");
+
+    }
+}
