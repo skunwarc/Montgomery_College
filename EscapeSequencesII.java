c4fa4622b8ef98a22604aa0049aef2bb5d03bff7
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-06-13 11:19:14 -0400

    Create EscapeSequencesII.java

---
+++ EscapeSequencesII.java
@@ -0,0 +1,10 @@
+public class EscapeSequencesII {
+
+    public static void main(String[] args) {
+        System.out.println("Cost     "+ "Quantity   "+ "Total   ");
+        System.out.println("$1,000.00   "+ " 4      "+ "$4,000.00");
+        System.out.println("$   50.00   "+ " 8    "+ "    $400.00");
+        System.out.println("________________________________");
+        System.out.println("TOTAL:      "+ " 12     "+ "$4,400.00");
+    }
+}
