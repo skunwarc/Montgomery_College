dae0c586cbc4c269215910affebdad67dbaed433
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-06-13 10:19:18 -0400

    Create StringCompletion.java

---
+++ StringCompletion.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class StringCompletion
+{
+    public static void main( String[] args )
+    {
+        String name;
+        String breed;
+        int age;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print( "Greetings. What is your pet's name? " );
+        name = keyboard.next();
+
+        System.out.print( "What kind of animal is " + name + "? " );
+
+        System.out.print( "How old is " + name + "? ");
+
+        age = keyboard.nextInt();
+
+
+        System.out.println("What is the breed name? ");
+        breed = keyboard.next();
+        System.out.println( name + " is your " + breed + " and it is " + age );
+
+        keyboard.close();
+    }
+}
+
+
+
+
+
+
+
+
+
