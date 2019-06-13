bf1b8fd49e01169f78dad3c7d0f1153de3cdd465
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-06-13 14:20:48 -0400

    Update PrintGrades.java

--- PrintGrades.java
+++ PrintGrades.java
@@ -1,3 +1,21 @@
+Q. Write a program that uses a while loop to prompt the user 
+to enter any number of exam scores 
+and the resulting program will print out the grade, A, B, C, D or F.
+
+If you're finished:
+Modify your program to print + or - after the grade. 
+A+ 97 to 100
+A 94 to 96
+A- 90 to 93
+B+ 87 to 89
+B 84 to 86
+B- 80 to 83
+C+ 77 to 79
+C 74 to 76
+C- 70 to 73
+D 60 to 69
+Go back to School if you get below 60
+
 
 import java.util.Scanner;
 
