b6f6253d74d0536a79f787602608ebecaf3695c9
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-06-13 11:20:08 -0400

    Update EscapeSequencesII.java

--- EscapeSequencesII.java
+++ EscapeSequencesII.java
@@ -4,7 +4,7 @@ public class EscapeSequencesII {
         System.out.println("Cost     "+ "Quantity   "+ "Total   ");
         System.out.println("$1,000.00   "+ " 4      "+ "$4,000.00");
         System.out.println("$   50.00   "+ " 8    "+ "    $400.00");
-        System.out.println("________________________________");
+        System.out.println("______________________________");
         System.out.println("TOTAL:      "+ " 12     "+ "$4,400.00");
     }
 }
