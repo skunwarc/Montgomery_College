1619c6ca5e6e03f4d6d1359c9326169b44bf4e14
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-06-14 08:03:48 -0400

    Update LoopDebug.java

--- LoopDebug.java
+++ LoopDebug.java
@@ -1,21 +1,35 @@
+/* 
+
+The following program should allow the user to input as many things as they want until they stop. 
+Each time it is supposed to repeat with what the user said, can you fix it?
+
+*/
+
 import java.util.Scanner;
 
 public class LoopDebug {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+	
 
-        Scanner keyboard = new Scanner(System.in);
+		
+		Scanner keyboard = new Scanner(System.in);
+		
         String userInput="";
+        
+        System.out.println("Enter the text: ");
+        
         userInput = keyboard.next();
 
-        while (!userInput.equals("") {
+        while (!userInput.equals("")) {
             
         
-            System.out.println(userInput);
-            System.out.println("Do you want to continue: (Y/N)?");
+            System.out.print(userInput+ " ");
+           
             userInput=keyboard.next();
 
         }
+		
+	}
 
-    }
 }
