e81bdd6f5fbe47067d67ab17afa95365ec9cf7b9
Sagar khatri <36431491+skunwarc@users.noreply.github.com>
2020-03-31 09:32:02 -0400

    Update StringAssign.java

--- StringAssign.java
+++ StringAssign.java
@@ -1,6 +1,6 @@
 public class StringAssign {
     public static void main(String args[]) {
-       String myName = "Sagar Kunwar Chhetri";
+       String myName = "Suman Ojha";
 
         System.out.println(myName);
     }
